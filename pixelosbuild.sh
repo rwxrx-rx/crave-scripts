65654f5e2e56031a1323179d95fcbabd54cbaa5a
RWXRXRX <akbarfaturochman@gmail.com>
2026-08-07 05:40:42 +0700

    Update pixelosbuild.sh

--- pixelosbuild.sh
+++ pixelosbuild.sh
@@ -24,13 +24,13 @@ echo "------------------------"
 
 # Trees Clone
 
-git clone https://github.com/dm700-devs/device_xiaomi_camellia -b lineage-23.2 device/xiaomi/camellia
-git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia -b main vendor/xiaomi/camellia
-git clone https://github.com/camellia-devs/kernel_xiaomi_mt6833 -b flyme kernel/xiaomi/camellia
-
-# Hardware dependencies
-git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-22.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.0 hardware/xiaomi
+git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b lineage-23.2 device/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 
 echo "---------------------"
 echo "Trees clone completed"

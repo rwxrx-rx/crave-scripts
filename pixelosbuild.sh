395b40d426421e752a984ec418491fdbc2f3ec65
RWXRXRX <akbarfaturochman@gmail.com>
2026-08-07 15:14:10 +0700

    Change build target from 'pixelos' to 'bacon'

--- pixelosbuild.sh
+++ pixelosbuild.sh
@@ -1,59 +1,65 @@
-# rom repo init
+#!/bin/bash
 
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
-
+# --- ROM Repo Init ---
 echo "-----------------------------"
-echo "Repo init cloned successfully"
+echo "Initializing Repo..."
 echo "-----------------------------"
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --depth=1
 
-# syncing
-
+# --- Syncing ---
 echo "-----------------------"
 echo "Starting to sync source"
 echo "-----------------------"
-
-/opt/crave/resync.sh
-
-/opt/crave/resync.sh
-
 /opt/crave/resync.sh
 
 echo "------------------------"
 echo "Source syncing completed"
 echo "------------------------"
 
-# Trees Clone
+# --- Trees Clone ---
+echo "-----------------------------"
+echo "Cloning Device & Vendor Trees"
+echo "-----------------------------"
 
-git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b lineage-23.2 device/xiaomi/camellia
-git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
-git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
-git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
-git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+# Hapus direktori lama jika ada untuk mencegah error "already exists"
+rm -rf device/xiaomi/camellia
+rm -rf vendor/xiaomi/camellia
+rm -rf device/xiaomi/camellia-kernel
+rm -rf vendor/mediatek/ims
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/mediatek
+rm -rf hardware/xiaomi
+
+# Clone dengan --depth=1 agar proses berjalan lebih cepat
+git clone --depth=1 https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b lineage-23.2 device/xiaomi/camellia
+git clone --depth=1 https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone --depth=1 https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone --depth=1 https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 
 echo "---------------------"
 echo "Trees clone completed"
 echo "---------------------"
 
-# build env
-
-. b*/e*
-
+# --- Build Environment Setup ---
 echo "---------------------------"
-echo "Build/envsetup.sh completed"
+echo "Setting up Build Environment"
 echo "---------------------------"
+source build/envsetup.sh
 
-# Lunch & Build
-
-breakfast camellia
-
+# --- Lunch & Build ---
 echo "----------------------------"
 echo "Starting PixelOS Compilation"
 echo "----------------------------"
 
-m pixelos
+# Menggunakan lunch target khas PixelOS/AOSP (bukan breakfast)
+lunch pixelos_camellia-userdebug
+
+# Target kompilasi standar untuk menghasilkan zip flashable
+m bacon
 
 echo "----------"
-echo "Done"
+echo "Build Done"
 echo "----------"
